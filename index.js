71349108101ed9c981883def79349ff96f71c69e
YAMADA Hiroaki <hy1193jp@outlook.jp>
2020-05-27 14:43:45 +0900

    20200527 adapt all-copy (Ctrl-A)

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 function handleDownload() {
   const regexp1 = /^(.*行　)/gm
-  var content = document.querySelector("#rtxt").value;
   var gno = document.querySelector("#gno").value;
-  console.log(gno);
-  content = content.replace(regexp1, '');
-  var blob = new Blob([ content ], { "type" : "text/plain" });
+  var content = document.querySelector("#rtxt").value;
+  var lines = content.split(/\r\n|\r|\n/);
 
+  result = '';
+  for ( var i=0; i < lines.length; i++ ) {
+    if ( lines[ i ].match(regexp1) ) {
+      result += lines[ i ].replace(regexp1, '') + "\r\n";
+    }
+  }
+
+  var blob = new Blob([ result ], { "type" : "text/plain" });
   if (window.navigator.msSaveBlob) { 
     window.navigator.msSaveBlob(blob, gno + ".txt"); 
     // msSaveOrOpenBlobの場合はファイルを保存せずに開ける
-    //window.navigator.msSaveOrOpenBlob(blob, "test.txt"); 
- } else {
+    // window.navigator.msSaveOrOpenBlob(blob, "test.txt"); 
+  } else {
     var e = document.getElementById("download");
-	e.href = window.URL.createObjectURL( blob );
-	e.download = gno + ".txt";
- }
+    e.href = window.URL.createObjectURL( blob );
+    e.download = gno + ".txt";
+  }
 }
