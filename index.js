d79adb36ef0a4c70eb545485c3aae60af20aa48c
YAMADA Hiroaki <hy1193jp@outlook.jp>
2020-06-02 19:23:20 +0900

    20200602 clear textarea after download

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function handleDownload() {
   const regexp1 = /^(.*行　)/gm
-  var gno = document.querySelector("#gno").value;
-  var content = document.querySelector("#rtxt").value;
-  var lines = content.split(/\r\n|\r|\n/);
+  var gno = document.querySelector("#gno");
+  var content = document.querySelector("#rtxt");
+  var lines = content.value.split(/\r\n|\r|\n/);
 
   result = '';
   for ( var i=0; i < lines.length; i++ ) {
@@ -13,12 +13,13 @@ function handleDownload() {
 
   var blob = new Blob([ result ], { "type" : "text/plain" });
   if (window.navigator.msSaveBlob) { 
-    window.navigator.msSaveBlob(blob, gno + ".txt"); 
+    window.navigator.msSaveBlob(blob, gno.vaule + ".txt"); 
     // msSaveOrOpenBlobの場合はファイルを保存せずに開ける
     // window.navigator.msSaveOrOpenBlob(blob, "test.txt"); 
   } else {
     var e = document.getElementById("download");
     e.href = window.URL.createObjectURL( blob );
-    e.download = gno + ".txt";
+    e.download = gno.value + ".txt";
   }
+  content.value = '';
 }
